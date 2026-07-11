b2eff67e4b8f6e50f85eb7978d43b37be8edcaa0
想想 <2998495730@qq.com>
2026-07-11 17:48:41 +0800

    创建一个tool

---
+++ tools/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/cloudwego/eino-ext/components/tool/browseruse"
+	"log"
+	"time"
+)
+
+func main() {
+	ctx := context.Background()
+	but, err := browseruse.NewBrowserUseTool(ctx, &browseruse.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	url := "https://www.google.com"
+	result, err := but.Execute(&browseruse.Param{
+		Action: browseruse.ActionGoToURL,
+		URL:    &url,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(result)
+	time.Sleep(10 * time.Second)
+	but.Cleanup()
+}
